996d04a4ae6894c6f393780ca84480d0fc8578c2
E-Molostova <molostova8973@gmail.com>
2021-09-02 18:32:23 +0300

    finish2

---
+++ index.js
@@ -0,0 +1,36 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const min = 0;
+const max = colors.length - 1;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+
+btnStart.addEventListener("click", onStart);
+btnStop.addEventListener("click", onStop);
+
+let id;
+//
+function onStart() {
+  btnStart.disabled = true;
+  id = setInterval(() => {
+    document.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(min, max)];
+  }, 1000);
+}
+//
+function onStop() {
+  clearInterval(id);
+  btnStart.disabled = false;
+}
